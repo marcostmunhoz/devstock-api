d4a43cbfaa06e83d7043f6584635775637a070c1
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2019-04-14 17:47:20 -0300

    ajustes UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,99 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
-
 class UsuarioController extends Controller
 {
-    public function showAll() {
-        $users = User::where('flg_status', '!=', 2)->get();
-
-        return response()->json([
-            'status' => 'ok',
-            'data'   => $users
-        ]);
-    }
-
-    public function show($id) {
-        $user = User::find($id);
-
-        try {
-            $user = User::find($id);
-
-            if (!$user || $user->flg_status == 2) {
-                return response()->json([
-                    'status'  => 'error',
-                    'message' => 'Usuário não encontrado.'
-                ], 404);
-            }
-        } catch (\Exception $ex) {
-            return response()->json([
-                'status'  => 'error',
-                'message' => $ex->getMessage()
-            ], 400);
-        }
-
-        return response()->json([ 
-            'status' => 'ok',
-            'data'   => $user 
-        ]);
-    }
-
-    public function update(Request $request, $id) {
-        $user = null;
-
-        try {
-            $user = User::find($id);
-            
-            if (!$user || $user->flg_status == 2) {
-                return response()->json([
-                    'status'  => 'error',
-                    'message' => 'Usuário não encontrado.'
-                ], 404);
-            }
-
-            $user->fill($request->all());
-            $user->save();
-        } catch (\Exception $ex) {
-            return response()->json([
-                'status'  => 'error',
-                'message' => $ex->getMessage()
-            ], 400);
-        }
-
-        return response()->json([
-            'status'  => 'ok',
-            'message' => 'Usuário editado com sucesso.',
-            'data'    => $user
-        ]);
-    }
-
-    public function delete($id) {
-        $user = null;
-
-        try {
-            $user = User::find($id);
-            
-            if (!$user || $user->flg_status == 2) {
-                return response()->json([
-                    'status'  => 'error',
-                    'message' => 'Usuário não encontrado.'
-                ], 404);
-            }
-
-            $user->flg_status = 2;
-            $user->save();
-        } catch (\Exception $ex) {
-            return response()->json([
-                'status'  => 'error',
-                'message' => $ex->getMessage()
-            ], 400);
-        }
-        
-        return response()->json([
-            'status'  => 'ok',
-            'message' => 'Usuário excluído com sucesso.'
-        ]);
+    public function __construct() {
+        $this->useStatusFlag = true;
+        $this->model = \App\User::class;
+        $this->friendlyName = 'Usuário';
+        $this->rules = [
+            'nm_usuario' => 'required|string|max:50',
+            'login'      => 'required|string|max:20|unique:users',
+            'password'   => 'required|string|min:6|confirmed',
+            'email'      => 'required|string|email|max:191|unique:users'
+        ];
     }
 }
