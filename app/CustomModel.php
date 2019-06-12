90448fdf3e756f2e229f1d34e59cd604f3ccc05a
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2019-06-12 01:29:21 -0300

    correcao offset tempo

--- app/CustomModel.php
+++ app/CustomModel.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class CustomModel extends Model {
     const CREATED_AT = 'dt_cadastro';
     const UPDATED_AT = 'dt_edicao';
+
+    public function getDtCadastroAttribute($value) {
+        return \Carbon\Carbon::parse($value, 'UTC')->timezone('America/Sao_Paulo');
+    }
+
+    public function getDtEdicaoAttribute($value) {
+        return \Carbon\Carbon::parse($value, 'UTC')->timezone('America/Sao_Paulo');
+    }
 }
